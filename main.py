4d1a5d8ea79b4127f7fec47f456cb72c94bfee89
Muzaffarbek <muzaffar@thinkland.uz>
2024-03-16 02:20:39 +0500

    key

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
-from typing import List
 import requests
 
 app = FastAPI()
 
-# OpenAPI tags
 app.openapi_tags = [
     {"name": "Search", "description": "Local bizneslarni qidirish bo'yicha yo'nalishlar"}
 ]
 
 RAPIDAPI_HOST = "local-business-data.p.rapidapi.com"
-RAPIDAPI_KEY = "4fb85f42afmsh20d726f3dbad5f8p1cfc77jsn6cb1616781c2"
 
 @app.get("/search", response_class=JSONResponse, tags=["Search"])
-async def search_hotels(query: str, limit: int = 20, lat: float = 37.359428, lng: float = -121.925337, zoom: int = 13, language: str = "en", region: str = "us"):
-    url = f"https://{RAPIDAPI_HOST}/search"
+async def search_hotels(query: str, rapidapi_key: str, limit: int = 20, lat: float = 37.359428, lng: float = -121.925337, zoom: int = 13, language: str = "en", region: str = "us"):
+    url = f"https://{RAPIDAPI_HOST}/search-nearby"
     headers = {
-        "X-RapidAPI-Key": RAPIDAPI_KEY,
+        "X-RapidAPI-Key": rapidapi_key,
         "X-RapidAPI-Host": RAPIDAPI_HOST
     }
     querystring = {
