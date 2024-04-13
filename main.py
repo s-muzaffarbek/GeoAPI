8c91554695c5df3e28e15f667d8baea5b0158b8a
Muzaffarbek <muzaffar@thinkland.uz>
2024-04-13 11:02:45 +0500

    update

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ async def search_hotels(query: str, rapidapi_key: str, limit: int = 20, lat: flo
 async def search_hotels(query: str, key: str):
     client = OpenAI(api_key=key)
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo-0301",
-        # model="gpt-4",
+        # model="gpt-3.5-turbo-0301",
+        model="gpt-3.5-turbo",
         messages=[
             {
                 "role": "user",
